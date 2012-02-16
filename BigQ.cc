aae9e2219592674c076fef3545675778769c8979
Paul Keim <pskeim@gmail.com>
2012-02-16 17:10:30 -0500

    First phase of P2-1 should be functional. Haven't tested anything, but should be functional (hahahahahahahahaha... oh god why). Phase 2 I still am not sure about.

--- BigQ.cc
+++ BigQ.cc
@@ -34,7 +34,6 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen)
  	// into the out pipe
 
     // finally shut down the out pipe
-	out.ShutDown ();
 }
 
 BigQ::~BigQ () {
@@ -48,48 +47,88 @@ void BigQ::FirstPhase(){
 	
 	//Assorted variable declaration. I'll sort them out into a more logical ordering sometime.
 	Page p; //Our holding page
+	//Variables for sorting and storing
 	Record readin; //Variable that works as a holder for when we're reading in from the pipe
-	vector<Record> run; //Okay, so, the "run" is a group of (NOT POINTERS TO RECORDS BUT THE ACTUAL OBJECTS) records (not a page, because there can be more than PageLen number of records)
-	//vector runs<vector<Record>; //Okay, this is the collection of runs.
 	Record temp; //Have to have another temp variable for when I'm moving records from the page to the vector
-	int runPages = 0; //Number of pages in our current run. If it ever matches runlen, we stop the reading in to sort and output the sorted records to the pipe
-	//File f; //This is the nice file I will be using to write stuff out to temp files with.
+	vector<Record> run; //Okay, so, the "run" is a group of (NOT POINTERS TO RECORDS BUT THE ACTUAL OBJECTS) records (not a page, because there can be more than PageLen number of records)
 	
-	bool extra = false;
-	while(input->Remove(&readin) == 1) //readin now contains a record. Where to store it? A page!
-	{
-		extra = true;
-		if(p.Append(&readin) == 0) //Okay, so now a problem: Append can fail. So we need to add handling.
-		{
-			//Now, we've got a full page, what do we put it in? Vector it in, captain.
+	int runPages = 0; //Number of pages in our current run. If it ever matches runlen, we stop the reading in to sort and output the sorted records to the pipe
+	
+	//Variables for writing out to file
+	f.Open(0,"temprecs");
+	int offset = 0; //Offset starts at 0 because File automaticaly thinks "offset+1" since first page is empty.
+	int n = 0; 
+
+	while(input->Remove(&readin) == 1){ //We have read a record in from the pipe
+		//We now attempt to append the record to the page
+		if(p.Append(&readin) == 0)
+		{ //If the append fails, we must remove the records from the page, and put them into the vector.
+			
 			while(p.GetFirst(&temp))
 			{
-				//Shove the records from the page into the run vector.
-				run.push_back(temp);
+				run.push_back(temp); //Put the record into our run vector
 			}
-			//Now the page is empty, we increment the number of pages that are in our current run
-			runPages++;
-			//And check to see if we've read in a total run
-			if(runPages == runlen)
+			runPages++; //Increment the number of run pages we currently have
+			
+			if(runPages == runlen-1)
 			{
+				//We've reached the number of pages allowed in a run, so we sort that shit.
+				
 				cout << "I am beginning to sort a run." << endl;
 				std::sort(run.begin(),run.end(), record_sorter(this->order)); //Sort the run
 				cout << "I have sorted a run." << endl;
-				//And now we need to write it out to file
-			}	
+				
+				
+				//And now we need to write it out to file	
+				//Okay, I need a page offset variable because if I'm writing these all to the same file
+				//Which I am, apparently, then I need to not accidentally overwrite anything.
+				//What I've got: A sorted vector, and a file.
+				//What I need: A page to store it in (p is empty! We can use that!)
+				
+				
+				for(n = 0; n < run.size(); n++){
+					if(p.Append(&run[n]) == 0){
+						//Failure to append, meaning we have to add the page to file, then start over
+						f.AddPage(&p, offset);
+						offset++;
+						
+						p.EmptyItOut(); //Empty and restart the process
+						p.Append(&run[n]);
+					}
+				}
+				//uhm, now we might have a page with a bunch of records in it that aren't in the file.
+				f.AddPage(p, offset);
+				offset++;
+				p.EmptyItOut();//Empty out the page, since we're about to add a new record to it.
+			}
+			//No matter if we've written it out to file or anything, we still need to append the new record!
 			p.Append(&readin);
 		}
 		
 	}
 	
+	//So, at this point, we have read all the records from the pipe. We have a page containing at least one record. SO:
+	while(p.GetFirst(&temp))
+		{
+			run.push_back(temp);
+		}
+	cout << "I am sorting the last run." <<endl;
+	std::sort(run.begin(),run.end(), record_sorter(this->order));
+	cout << "I have sorted the last run." << endl;
+	for(n = 0; n < run.size(); n++){
+		if(p.Append(&run[n]) == 0){
+			//Failure to append, meaning we have to add the page to file, then start over
+			f.AddPage(&p, offset);
+			offset++;
+			
+			p.EmptyItOut(); //Empty and restart the process
+			p.Append(&run[n]);
+		}
+	}
+	//uhm, now we might have a page with a bunch of records in it that aren't in the file.
+	f.AddPage(p, offset);
+	offset++;
+	p.EmptyItOut();//Empty out the page, since we're about to add a new record to it.
 	
-	//We still might have a page of records (if the number of records % size of page = 0, but number of records % runlen*pageSize != 0, or something)
-	//So we need to check that, and sort/stuff our vector
-	//Worry about that later. Keep thinking about how this is supposed to be set up
-	
-	//ANYWAYS
-	
-	
-
-
+	//So at the end of this, f is our link to an opened file with all our runs in it. Now we need to enter phase 2
 }
